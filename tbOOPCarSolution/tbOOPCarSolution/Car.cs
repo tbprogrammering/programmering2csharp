bbcc74ea43c626103b8fa81d2ec585ab684d57aa
LethalRooster <petersson@outlook.com>
2022-01-27 11:17:03 +0100

    New

---
+++ tbOOPCarSolution/tbOOPCarSolution/Car.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tbOOPCarSolution
+{
+    class Car
+    {
+        public string brand;
+        public string model;
+        public int year;
+        public string fuel;
+        public string license_plate;
+        private float speed;
+        public string colour;
+
+        public Car()
+        {
+            brand = "undefined";
+            model = "undefined";
+            year = 0;
+            fuel = "undefined";
+            license_plate = "undefined";
+            speed = 0f;
+            colour = "undefined";
+        }
+
+        public Car(string brand, string model, string colour,
+            int year, string fuel, string license_plate)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.colour = colour;
+            this.year = year;
+            this.fuel = fuel;
+            this.license_plate = license_plate;
+            this.speed = 0;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{license_plate} {brand} {model} {colour} {year} {fuel} {speed}");
+        }
+
+        public void Horn()
+        {
+            //HEJ
+            Console.WriteLine($"{license_plate} Tut tut");
+        }
+
+        public float Accelerate()
+        {
+            speed += 10f;
+            return speed;
+        }
+
+        public float Brake()
+        {
+            speed -= 10f;
+            return speed;
+        }
+
+        public float GetSpeed()
+        {
+            return speed;
+        }
+
+
+    }
+}
---
+++ tbOOPCarSolution/tbOOPCarSolution/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tbOOPCarSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car();
+            Car c2 = new Car("Volvo", "V90", "Röd", 2020, "Diesel", "ABC123");
+
+            //Console.WriteLine(c1.brand);
+            //Console.WriteLine(c2.brand);
+
+            c1.Print();
+            c2.Print();
+
+            c1.Horn();
+            c2.Horn();
+
+            c2.Accelerate();
+            c2.Accelerate();
+            
+
+            Console.WriteLine(c2.GetSpeed());
+        }
+    }
+}

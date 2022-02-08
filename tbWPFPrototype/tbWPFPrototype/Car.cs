7dc38b1c6679795196beb079b4b6c0a0845904eb
tbprogrammering <jonas.stroberg@helsingborg.se>
2022-02-08 10:29:03 +0100

    Exempel med WPF

---
+++ tbWPFPrototype/tbWPFPrototype/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tbWPFPrototype
+{
+    class Car /*: IComparer*/
+    {
+        string _license_plate;
+        string _brand;
+        string _model;
+        int _year;
+        string _colour;
+        string _fuel;
+        DateTime? _reg_date;
+                 
+        public string License_Plate { get => _license_plate; }
+        public string Brand { get => _brand; }
+        public string Model { get => _model; }
+        public int Year { get => _year; }
+        public string Colour { get => _colour; }
+        public string Fuel { get => _fuel; }
+        public DateTime? RegDate { get => _reg_date; }
+
+        public Car(string license_plate, string brand, string model, int year, string colour, string fuel, DateTime?  reg_date)
+        {
+            _license_plate = license_plate;
+            _brand = brand;
+            _model = model;
+            _year = year;
+            _colour = colour;
+            _fuel = fuel;
+            _reg_date = reg_date;
+        }
+
+        /*public int Compare(object x, object y)
+        {
+            Car c1 = (Car)x;
+            Car c2 = (Car)y;
+
+            return c1.License_Plate.CompareTo(c2.License_Plate);
+        }*/
+
+    }
+} 
---
+++ tbWPFPrototype/tbWPFPrototype/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace tbWPFPrototype
+{
+    public partial class MainWindow : Window
+    {
+        static Random rand = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dpRegDate.SelectedDate = DateTime.Now;
+            cbBrands.ItemsSource = File.ReadAllLines(@"..\..\brands.txt");
+
+            for (int i = DateTime.Now.Year + 1; i >= 1950; i--)
+            {
+                cbYear.Items.Add(i);
+            }
+            
+        }
+
+        private void btnAddNewCar_Click(object sender, RoutedEventArgs e)
+        {
+            string license = txtLicensePlate.Text;
+            bool add_car = true;
+            if (license.CompareTo("") != 0 && license.Length > 1 && license.Length < 8)
+            {
+                foreach (Car c in lvCars.Items)
+                {
+                    if (c.License_Plate.CompareTo(license) == 0)
+                    {
+                        add_car = false;
+                        MessageBox.Show($"Registreringsnummer finns redan i systemet");
+                        break;
+                    }
+                }
+
+                if (add_car)
+                {
+                    lvCars.Items.Add(new Car(license, cbBrands.Text, txtModel.Text, int.Parse(cbYear.Text), txtColour.Text, cbFuel.Text, dpRegDate.SelectedDate));
+                    CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvCars.Items);
+                    view.SortDescriptions.Add(new SortDescription("License_Plate", ListSortDirection.Ascending));
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Felaktigt registreringsnummer");
+            }
+           
+        }
+
+        private void RemoveCar()
+        {
+            for (int i = 0; i < lvCars.SelectedItems.Count; i++)
+            {
+                if (lvCars.SelectedIndex >= 0 && MessageBox.Show($"Ta bort bil med regnr {((Car)lvCars.SelectedItems[i]).License_Plate}? ", "Ta bort bil?", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    lvCars.Items.RemoveAt(lvCars.Items.IndexOf(lvCars.SelectedItems[i]));
+                    i--;
+                }
+            }            
+        }
+
+        private void btnRemoveCar_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveCar();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                RemoveCar();
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
